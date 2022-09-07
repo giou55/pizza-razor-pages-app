ecd1d1b1c34a9b4fb075d58078405f4bbebb796f
giou55 <giourmetakis00@gmail.com>
2022-09-07 11:51:59 +0300

    changes

--- NET-razor-pages-application/Pages/Forms/CustomPizza.cshtml.cs
+++ NET-razor-pages-application/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,28 @@ namespace NET_razor_pages_application.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult onPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Beef) PizzaPrice += 1;
+
+            return RedirectToPage(
+                "/Chechout/Checkout", 
+                new { Pizza.PizzaName, PizzaPrice }
+            );
         }
     }
 }
